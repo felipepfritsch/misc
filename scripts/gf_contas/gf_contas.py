ca9bb03e2d9f9b6e3987ce38c340cc86fa376ae3
Felipe Fritsch <felipepfritsch@outlook.com>
2022-01-12 18:45:39 -0300

    first commit

--- scripts/gf_contas/gf_contas.py
+++ scripts/gf_contas/gf_contas.py
@@ -10,22 +10,16 @@ file_path = file_dir + '\\' + file_name
 init_df = pd.read_excel(file_path)
 
 # Reformatting data
-name_map = {'L': 'laura',
-            'I': 'itunes',
-            'F': 'felipe',
-            'M': 'fafa',
-            'UB': 'uber',
-            'U': 'unknown'}
+name_map = {'L': 'Laura', 'I': 'Itunes', 'F': 'Felipe',
+            'M': 'Fafa', 'G': 'Gabriel', 'UB': 'Uber',
+            'UN': 'Unknown'}
 init_df['person'] = init_df['person'].map(name_map)
-init_df['expense_type'] = init_df['expense_type'].apply(lambda x: x.lower())
-
 df = init_df
 
-
 # Analise
-include_uber = False
-df = df if include_uber else df.loc[df['gasto'] != 'Uber']
-total_pp = df.groupby('pessoa').apply(lambda x: x['valor'].sum())
+include_uber = True
+df = df if include_uber else df.loc[df['person'] != 'Uber']
+total_pp = df.groupby('person').apply(lambda x: x['value'].sum())
 
-felipe_df = df.loc[df['pessoa'] == 'Felipe']
-felipe_df.groupby('gasto')['valor'].sum()
+felipe_df = df.loc[df['person'] == 'Felipe']
+felipe_df.groupby('expense_type')['value'].sum()
